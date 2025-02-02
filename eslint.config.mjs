0874206606bd409cd52aefec8daacefe2d783389
Fabricio <fabriciopviana@outlook.com>
2025-02-02 12:18:56 -0300

    adiciona funcionalidades CRUD para recados e configurações de estilo de código

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,9 +30,22 @@ export default tseslint.config(
             "prettier/prettier": [
             "error",
             {
-                "endOfLine": "auto",
+                "arrowParens": "avoid",
+                "bracketSpacing": true,
+                "htmlWhitespaceSensitivity": "css",
+                "insertPragma": false,
+                "jsxBracketSameLine": false,
+                "jsxSingleQuote": true,
+                "printWidth": 80,
+                "proseWrap": "always",
+                "quoteProps": "as-needed",
+                "requirePragma": false,
+                "semi": true,
                 "singleQuote": true,
-            }
+                "tabWidth": 2,
+                "trailingComma": "all",
+                "useTabs": false
+              }
             ],
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/no-floating-promises': 'warn',
--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,19 +1,45 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { RecadosService } from './recados.service';
 
 @Controller('recados')
 export class RecadosController {
+  constructor(private readonly recadosService: RecadosService) {}
+
+  @HttpCode(HttpStatus.OK)
   @Get()
-  findAll() {
-    return 'todos os recados'
+  findAll(@Query() pagination: { limit: number; offset: number }) {
+    const { limit = 10, offset = 0 } = pagination;
+    return 'todos os recados com limit ' + limit + ' e offset' + offset;
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `um recado de id ${id}`
+    return `um recado de id ${id}`;
   }
 
   @Post()
   create(@Body() body: any) {
-    return `criado um recado ${JSON.stringify(body)}`
+    return `criado um recado ${JSON.stringify(body)}`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return `atualizado o recado de id ${id} com ${JSON.stringify(body)}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `deletado o recado de id ${id}`;
   }
 }
